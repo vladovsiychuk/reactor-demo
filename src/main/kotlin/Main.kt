ef332b858de6a542600b7f03aac784c5c4c463f6
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2022-11-02 15:22:13 +0200

    add tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,29 +1,62 @@
 import reactor.core.publisher.Mono
+import reactor.core.publisher.switchIfEmpty
 
 fun main(args: Array<String>) {
-//        testErrorHandling()
+    //    testErrorHandling()
     testCompletable()
+    //    testEmpty()
 }
 
-private fun testCompletable() {
-    completableMethod()
-        .map {
-            println("inside map")
+fun testEmpty() {
+    Mono.empty<Int>()
+        .doOnError {
+            println("inside error subscribe")
+        }
+        .switchIfEmpty {
+            Mono.error(NoSuchElementException("not found exception"))
         }
         .subscribe()
 }
 
+private fun testCompletable() {
+    completableMethod()
+        .then(printSomething() { myString ->
+            println(myString)
+            Mono.just("Inside then")
+                .map{
+                    println(it)
+                    it
+                }
+        })
+        .subscribe()
+}
+
+fun printSomething(def: (abc: String) -> Mono<String>): Mono<String> {
+    println("A")
+    return def("D")
+}
+
 private fun completableMethod(): Mono<Unit> {
-    return if (true) {
-        Mono.just(Unit)
+    return if (false) {
+        Mono.empty()
     } else {
         Mono.error(Exception("Some exception"))
     }
 }
 
 private fun testErrorHandling() {
-    Mono.error<Int>(Exception("some error"))
-        .subscribe {
-            println(it)
+    Mono.just(1)
+        .flatMap {
+            Mono.error<Exception>(Exception("A"))
+                .doOnError {
+                    println("B")
+                }
+                .map {
+                    println("C")
+                    it
+                }
         }
+        .subscribe({}, {
+            println("D")
+        })
 }
