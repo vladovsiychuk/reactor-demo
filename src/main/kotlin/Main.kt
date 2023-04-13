cde77853d90ef1fa3fca58c545a211a427ec143e
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-04-13 19:00:17 +0300

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,38 +1,24 @@
 import reactor.core.publisher.Mono
-import reactor.core.publisher.switchIfEmpty
-import java.sql.Wrapper
+import kotlin.Exception
 
 fun main(args: Array<String>) {
 
-    test()
-}
-
-fun test() {
-    Mono.just("A")
-        .flatMap {
-            monoVoid(it)
+    myFunc()
+        .subscribe {
+            println("My string is: $it")
         }
-        .then(
-            Mono.just("B")
-                .flatMap {
-                  Mono.just(0)
-//                  monoVoid(it)
-                }
-                .switchIfEmpty(
-                    Mono.just("C")
-                        .map {
-                            println(it)
-                        }
-                        .flatMap {
-                            Mono.empty()
-                        }
-                )
-        )
-        .subscribe()
 }
 
-fun monoVoid(value: String): Mono<Void> {
-    println(value)
-
-    return Mono.empty()
+private fun myFunc(): Mono<String> {
+    return Mono.zip(
+        Mono.just("a"),
+        Mono.empty<String>()
+            .switchIfEmpty(Mono.error(Exception("exception occurred")))
+    )
+        .map {
+            "print ${it.t1} and ${it.t2}"
+        }
+        .doOnSuccess {
+            println("something something")
+        }
 }
